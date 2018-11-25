401147dfc5959a1db78a6811463305cb7531459f
Geoffrey Remacle <geoffreyremacle@outlook.fr>
2018-11-25 14:59:53 +0100

    modif

--- t.py
+++ t.py
@@ -1,13 +1,6 @@
-valeur =[0,0,2,3,4,5,6,7,8,9,10,"Valet","Dame","Roi","As"]
-couleur = ("coeur", "carreau","trèfle","pique")
 
-liste = []
-for j in range(4):
-	for i in range(13):
-		liste.append((i+2,j))
 
-print(liste)
 
-print("{} de {}".format(valeur[liste[12][0]],couleur[liste[12][1]]))
+
 
 
